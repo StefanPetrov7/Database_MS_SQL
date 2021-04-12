7887648f56a48304907d1d076c8c3fee698c9e10
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-04-12 14:20:01 +0300

    Add files via upload

--- Indices_And_Data_Aggregation/Exercises_Indices_and_Data_Aggregation.sql
+++ Indices_And_Data_Aggregation/Exercises_Indices_and_Data_Aggregation.sql
@@ -1,5 +1,7 @@
 -- Exercises: Indices and Data Aggregation
 
+-- Using DB Gringotts
+
 -- 1. Records’ Count
 
 SELECT COUNT(*) AS [Count]
@@ -25,7 +27,6 @@ FROM WizzardDeposits
 GROUP BY [DepositGroup]
 
 -- 4. * Smallest Deposit Group Per Magic Wand Size
-
 
 SELECT [DepositGroup] FROM
 (
@@ -96,3 +97,130 @@ FROM
     FROM WizzardDeposits
 ) AS x
 GROUP BY [AgeGroup]
+
+-- 10. First Letter
+
+SELECT [FirstLetter] FROM 
+(
+    SELECT LEFT([FirstName],1) AS [FirstLetter]
+    FROM WizzardDeposits
+    WHERE [DepositGroup] = 'Troll Chest'
+    GROUP BY [FirstName]
+) As x
+GROUP BY [FirstLetter]
+
+-- 11. Average Interest 
+
+SELECT [DepositGroup],
+    [IsDepositExpired],
+    AVG([DepositInterest])
+FROM WizzardDeposits
+WHERE [DepositStartDate] >= '01/01/1985' 
+GROUP BY [DepositGroup], [IsDepositExpired]
+ORDER BY [DepositGroup] DESC, [IsDepositExpired]
+
+-- 12. * Rich Wizard, Poor Wizard
+
+SELECT SUM(Guest.[DepositAmount] - Host.[DepositAmount])AS [SumDifference]
+FROM WizzardDeposits as Host
+JOIN WizzardDeposits as Guest ON  Guest.Id+1 = Host.Id
+
+-- 13. Departments Total Salaries
+
+-- Using DB SoftUni
+
+SELECT [DepartmentID],
+SUM(Salary) AS [TotalSalary] 
+FROM Employees
+GROUP BY [DepartmentID]
+ORDER BY [DepartmentID]
+
+-- 14. Employees Minimum Salaries
+
+SELECT [DepartmentID],
+    MIN(Salary) AS [MinimumSalary]
+FROM Employees
+WHERE [DepartmentID] IN(2,5,7) AND [HireDate] > '01/01/2000'
+GROUP BY [DepartmentID]
+
+-- 15. Employees Average Salaries
+
+SELECT * INTO EmployeeUpdate 
+FROM Employees 
+WHERE [Salary] > 30000
+
+DELETE FROM EmployeeUpdate 
+WHERE [ManagerID] = 42
+
+UPDATE EmployeeUpdate 
+SET [Salary] = [Salary] + 5000
+WHERE [DepartmentID] = 1
+
+SELECT [DepartmentID],
+AVG([Salary]) AS [AverageSalary]
+FROM EmployeeUpdate
+GROUP BY [DepartmentID]
+
+DROP TABlE EmployeeUpdate
+
+-- 16. Employees Maximum Salaries
+
+SELECT [DepartmentID],
+MAX([Salary]) AS [MaxSalary]
+FROM Employees
+GROUP BY [DepartmentID]
+HAVING MAX([Salary]) NOT BETWEEN 30000 AND 70000
+
+-- 17. Employees Count Salaries
+
+SELECT COUNT(*) AS [Count]
+FROM Employees
+WHERE [ManagerID] IS NULL
+
+-- 18. *3rd Highest Salary
+
+SELECT 
+x.[DepartmentID],
+x.[Salary] AS [ThirdHighestSalary]
+FROM 
+(
+    SELECT [DepartmentID],[Salary],
+        DENSE_RANK() OVER (PARTITION BY [DepartmentID] ORDER BY MAX([Salary]) DESC) AS [Ranked]
+    FROM Employees
+    GROUP BY [DepartmentID], [Salary]
+) As x
+WHERE x.[Ranked] = 3
+
+--19. **Salary Challenge
+
+SELECT TOP(10) [FirstName],
+    [LastName],
+    [DepartmentID]
+FROM Employees AS Emp
+WHERE [Salary] > 
+                (
+                    SELECT    AVG([Salary])
+                    FROM Employees
+                    WHERE [DepartmentID] = emp.[DepartmentID]
+                    GROUP BY [DepartmentID]
+                )
+ORDER BY [DepartmentID]
+                
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
